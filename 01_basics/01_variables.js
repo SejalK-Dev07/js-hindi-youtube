7be8eea9a5b76c3ae55756de939e99f84b77053a
SejalK-Dev07 <sejalrode07@gmail.com>
2025-06-15 06:12:36 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = (22447);
+console.log(accountId);
+
+let accountEmail = ("sejj@gmail.com");
+var accountPassword = (767);
+let accountName = ("Sejj");
+let accountCity = ("Pune");
+
+console.table([accountEmail, accountPassword, accountName, accountCity]);
+
